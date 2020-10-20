5987222a6c131a2a54d81252befc19f570cc5d07
r2on11 <hulajrron@gmail.com>
2020-10-20 18:07:17 +0200

    new project

--- assets/js/porto.js
+++ assets/js/porto.js
@@ -2,13 +2,7 @@ $('.nav-link').on('click',function() {
     $('.navbar-collapse').collapse('hide');
   });
 
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 150) {
-    $('.navbar').addClass("navscr")
-    } else {
-         $('.navbar').removeClass("navscr")
-        }
-        });     
+   
 
 $(document).ready(function() {
      $('.navbar-toggler').click(function() {
