2753ddf516257c9b4083245d8e041512baa22051
r2on11 <hulajrron@gmail.com>
2020-10-21 16:55:03 +0200

    nav scroll fix

--- assets/js/porto.js
+++ assets/js/porto.js
@@ -7,3 +7,10 @@ $('.nav-link').on('click',function() {
 $(document).ready(function() {
      $('.navbar-toggler').click(function() {
         $("nav").toggleClass("navbar-mobclick",2000);});})
+
+$(function () {
+  $(document).scroll(function () {
+	  var $nav = $(".navbar");
+	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+	});
+});
